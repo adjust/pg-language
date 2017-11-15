2d2c0a0ea028d7adabd616222194fec593b395fa
Chris Travers <chris.travers@gmail.com>
2017-11-15 14:16:22 +0100

    Fixing ordering of autobuilt extension files

--- sql/language--0.0.2.sql
+++ sql/language--0.0.2.sql
@@ -1,27 +1,5 @@
 
 
-
-DO $$
-DECLARE version_num integer;
-BEGIN
-  SELECT current_setting('server_version_num') INTO STRICT version_num;
-  IF version_num > 90600 THEN
-	EXECUTE $E$ ALTER FUNCTION language_in(cstring) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_out(language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_recv(internal) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_send(language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_eq(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_neq(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_lt(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_le(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_gt(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_ge(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION language_cmp(language, language) PARALLEL SAFE $E$;
-	EXECUTE $E$ ALTER FUNCTION hash_language(language) PARALLEL SAFE $E$;
-  END IF;
-END;
-$$;
-
 CREATE TYPE language;
 
 CREATE FUNCTION supported_languages()
@@ -202,3 +180,25 @@ CREATE OPERATOR CLASS language_ops
     DEFAULT FOR TYPE language USING hash AS
         OPERATOR        1       = ,
         FUNCTION        1       hash_language(language);
+
+DO $$
+DECLARE version_num integer;
+BEGIN
+  SELECT current_setting('server_version_num') INTO STRICT version_num;
+  IF version_num > 90600 THEN
+	EXECUTE $E$ ALTER FUNCTION language_in(cstring) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_out(language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_recv(internal) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_send(language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_eq(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_neq(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_lt(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_le(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_gt(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_ge(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION language_cmp(language, language) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION hash_language(language) PARALLEL SAFE $E$;
+  END IF;
+END;
+$$;
+
